decfa62cc3d9be3aaeb66102d037d6066a0b9ba0
RP <rahulpahuja2015@gmail.com>
2021-07-04 15:14:41 +0530

    Initial Spring Setup Commit with Maven Support for Dependencies in the POM

---
+++ src/test/java/com/server/decovid/DecovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.decovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
